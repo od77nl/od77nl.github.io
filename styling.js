fa64547cbd34d24d1a1c921a1c7925fcf463822d
od77nl <spap0002@hz.nl>
2022-10-11 09:09:46 +0200

    assignment last update

--- styling.js
+++ styling.js
@@ -8,4 +8,13 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
-  
+  window.onload = function(){
+    var elm = document.querySelector('#progress');
+    setInterval(function(){
+      if(!elm.innerHTML.match(/100%/gi)){
+        elm.innerHTML = (parseInt(elm.innerHTML) + 1) + '%';
+      } else {
+        clearInterval();
+      }
+    }, 18)
+  }
